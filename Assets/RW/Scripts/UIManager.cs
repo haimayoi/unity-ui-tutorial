edea693f3a080fa7c1c6f89d2e2eeb364fb07caf
Hải Vân <k34ctqer@gmail.com>
2025-11-07 08:30:49 +0700

    done part 3.2

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -8,6 +8,8 @@ public class UIManager : MonoBehaviour
     public Animator startButton;
     public Animator settingsButton;
     public Animator dialog;
+    public Animator contentPanel;
+    public Animator gearImage;
 
     public void StartGame() 
     {
@@ -26,5 +28,12 @@ public class UIManager : MonoBehaviour
         startButton.SetBool("isHidden", false);
         settingsButton.SetBool("isHidden", false);
         dialog.SetBool("isHidden", true);
+    }
+
+    public void ToggleMenu() 
+    {
+        bool isHidden = contentPanel.GetBool("isHidden");
+        contentPanel.SetBool("isHidden", !isHidden);
+        gearImage.SetBool("isHidden", !isHidden);
     }
 }
