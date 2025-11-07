d5e58213d5209c498f576fc451ec8fdabb0efa3e
Hải Vân <k34ctqer@gmail.com>
2025-11-07 07:47:31 +0700

    done part 2.3

--- Assets/RW/Scripts/UIManager.cs
+++ Assets/RW/Scripts/UIManager.cs
@@ -7,6 +7,7 @@ public class UIManager : MonoBehaviour
 {
     public Animator startButton;
     public Animator settingsButton;
+    public Animator dialog;
 
     public void StartGame() 
     {
@@ -17,5 +18,13 @@ public class UIManager : MonoBehaviour
     {
         startButton.SetBool("isHidden", true);
         settingsButton.SetBool("isHidden", true);
+        dialog.SetBool("isHidden", false);
+    }
+
+    public void CloseSettings() 
+    {
+        startButton.SetBool("isHidden", false);
+        settingsButton.SetBool("isHidden", false);
+        dialog.SetBool("isHidden", true);
     }
 }
